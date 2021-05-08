63e9ecef9eb13dd506188493ea9caeaeeb5d164d
Henry <hablandon750@outlook.com>
2021-05-08 00:09:38 -0700

    added files
     started boiler code for express app.

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+
+/* Instantiate Express app */
+const app = express();
+
+/* Setup view engine, we are using pug templates*/
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// url encoded parser
+app.use(express.urlencoded({ entended: false }));
+// app.use(cookieParser()); not sure if i need cookie parser yet.
+
+// sets the public folder as static in the route 'static'
+app.use('/static', express.static('public'));
+
+// root route need to test
+app.get('/', (req, res) => {
+    res.send('hello from root route');
+});
+
+app.listen(3000);
