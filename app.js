d9d6729867b0d9c32ac86a45aad12ce27eba2ae3
Henry <hablandon750@outlook.com>
2021-05-09 22:52:44 -0700

    added project json file
    I still need to add live link, also, I need to figure out the size of pictures

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const cookieParser = require('cookie-parser');
 
 /* Instantiate Express app */
 const app = express();
 
+app.use(cookieParser());
 /* Setup view engine, we are using pug templates*/
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
